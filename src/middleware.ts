c1d36670673974b65ed40a13542a824144bfeb32
Paul Mac <plipp2804@gmail.com>
2024-04-25 20:12:24 -0700

    middleware stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default clerkMiddleware((auth, request) => {
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(.*)"],
 };
